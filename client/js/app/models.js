ba500e2a952339461fb21c676373097a85e4c6f6
Alex McLeod <alxmcleod@gmail.com>
2013-12-10 10:26:44 +1100

    Restructured application

--- client/js/app/models.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('reviseModels', []).
-  factory('Course', ['$resource', 'API_ROOT', function($resource, API_ROOT) {
-      return $resource(API_ROOT + 'courses/:courseId', {courseId: '@courseId', format: 'json'}, {
-          get: {method: 'GET', isArray: true}
-      });
-  }]);
--- server/collections.js
+++
@@ -1,5 +0,0 @@
-// TODO Add a schema for each document type. 
-
-Courses = new Meteor.Collection('courses');
-
-Reviews = new Meteor.Collection('reviews');
